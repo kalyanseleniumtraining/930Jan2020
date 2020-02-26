74691be0fccd014f6e661fff7f8ccfd7f3ef2482
S@f' <S@f'@Sf-PC>
2020-02-26 10:30:08 +0530

    second commit

--- FirstJavaProject/src/practice/AutoItExample.java
+++ FirstJavaProject/src/practice/AutoItExample.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AutoItExample {
 	
 	public static void main(String[] args) throws Exception {
+		//this is to verify the code in the git hub
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.convertfiles.com/");
